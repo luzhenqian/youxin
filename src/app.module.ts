9d3944de805ab9155322d1c4fbc959d7ce661973
luzhenqian <15753140326@163.com>
2021-05-26 23:44:26 +0800

    feat: project

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,19 +4,18 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { ProjectsModule } from './projects/projects.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(
-      process.env.MONGODB_URI,
-      {
-        useFindAndModify: false,
-      },
-    ),
+    MongooseModule.forRoot(process.env.MONGODB_URI, {
+      useFindAndModify: false,
+    }),
     AuthModule,
+    ProjectsModule,
   ],
 })
 export class AppModule {}
---
+++ src/projects/schemas/project.schema.ts
@@ -0,0 +1,15 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type ProjectDocument = Project & Document;
+
+@Schema()
+export class Project {
+  @Prop()
+  name: string;
+
+  @Prop()
+  desc: string;
+}
+
+export const ProjectSchema = SchemaFactory.createForClass(Project);
